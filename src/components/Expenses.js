013be1984fae1c6b44e50d93ca864a35cd66fc3b
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-14 15:01:10 +0200

    expenses layout

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import classes from './css/expenses.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 
 const Expenses = () => {
   return (
     <section className={classes.expenses}>
       <h2 className={classes.header}>Expenses</h2>
+      <FontAwesomeIcon
+        icon={faCirclePlus}
+        className={classes.add}
+        onClick={() => console.log('hejka')}
+      />
+      <div className={classes.expensesContainer}></div>
     </section>
   );
 };
