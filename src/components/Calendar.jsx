136c56ceea1d4ccf105e6bf1e7c29cf52fe1433e
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-17 15:20:03 +0200

    changing date of expense using calendar

--- src/components/Calendar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './css/calendar.module.css';
-
-const Calendar = (props) => {
-  return <div></div>;
-};
-
-export default Calendar;
--- src/components/ExpensesInput.jsx
+++ src/components/ExpensesInput.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import { useState } from 'react';
 import classes from './css/expensesInput.module.css';
+import './css/calendar.css';
 // font awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlus, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 // helper functions
 import { getCurrentDay } from './getCurrentDay';
 // components
-import Calendar from './Calendar';
+import Calendar from 'react-calendar';
 
 const ExpensesInput = (props) => {
   const [formData, setFormData] = useState({
     title: '',
     money: '',
     category: '',
-    date: getCurrentDay(),
   });
   const [isChangingDate, setIsChangingDate] = useState(false);
+  const [date, setDate] = useState(getCurrentDay());
+
+  const changingDate = () => {
+    setIsChangingDate((prevState) => !prevState);
+  };
+
+  const changeDate = (value) => {
+    let day = value.getDate();
+    let month = value.getMonth() + 1;
+
+    if (day < 10) {
+      day = `0${day}`;
+    }
+    if (month < 10) {
+      month = `0${month}`;
+    }
+
+    setDate(`${day}/${month}`);
+    setIsChangingDate((prevState) => !prevState);
+  };
 
   const handleChange = (e) => {
     setFormData((prevState) => {
@@ -68,14 +88,23 @@ const ExpensesInput = (props) => {
         <option value='housing'>Housing</option>
         <option value='investments'>Investments</option>
       </select>
-      <div className={classes.date}>
-        {formData.date}
+      <div className={classes.date} onClick={changingDate}>
+        {date}
         <FontAwesomeIcon icon={faAngleDown} className={classes.dateArrow} />
       </div>
       <button className={classes.submit}>
         <FontAwesomeIcon icon={faCirclePlus} className={classes.add} />
       </button>
-      {isChangingDate ? <Calendar /> : null}
+      {isChangingDate ? (
+        <Calendar
+          className={classes.calendar}
+          tileClassName={classes.day}
+          showNavigation={false}
+          showNeighboringMonth={false}
+          locale={'en-EN'}
+          onChange={(value, event) => changeDate(value)}
+        />
+      ) : null}
     </form>
   );
 };
